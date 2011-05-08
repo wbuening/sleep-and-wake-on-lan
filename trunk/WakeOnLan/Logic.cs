9e0492b40965148cb8f0fc01769a4d2b57524624
aleksandrov.stas@gmail.com <aleksandrov.stas@gmail.com@d7ea39bf-fc85-07e9-c031-cf3062c2ab2b>
2011-05-08 09:31:13 +0000

    First revision.

---
+++ trunk/WakeOnLan/Logic.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+
+namespace WakeOnLan
+{
+    public static class Logic
+    {
+        public static void Send(byte[] packet, string text)
+        {
+            var udpClient = new UdpClient();
+            udpClient.Send(packet, packet.Length, new IPEndPoint(IPAddress.Parse(text), 9));
+            udpClient.Close();
+        }
+
+        public static byte[] GetMagicPacket(string s)
+        {
+            var arr = new List<byte>(102);
+
+            string[] macs = s.Split(' ', ':', '-');
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr.Add(0xff);
+            }
+
+            for (int j = 0; j < 16; j++)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    arr.Add(Convert.ToByte(macs[i], 16));
+                }
+            }
+
+            return arr.ToArray();
+        }
+
+        // "Antimagic" packet here is the magic packet with reversed mac address bytes.
+        public static byte[] GetAntiMagicPacket(string s)
+        {
+            var arr = new List<byte>(102);
+
+            string[] macs = s.Split(' ', ':', '-');
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr.Add(0xff);
+            }
+
+            for (int j = 0; j < 16; j++)
+            {
+                for (int i = 0; i < 6; i++)
+                {
+                    arr.Add(Convert.ToByte(macs[5 - i], 16));
+                }
+            }
+
+            return arr.ToArray();
+        }
+    }
+}
---
+++ trunk/WakeOnLan/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace WakeOnLan
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private const string Path = "Settings.ini";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<string> list = Settings.Load(Path);
+            if (list.Count == 0)
+            {
+                list.Add("");
+                list.Add("");
+                Settings.Save(Path, list);
+            }
+
+            textIP.Text = list[0];
+            textMAC.Text = list[1];
+        }
+
+        private void ButtonWakeClick(object sender, RoutedEventArgs e)
+        {
+            Logic.Send(Logic.GetMagicPacket(textMAC.Text), textIP.Text);
+        }
+
+        private void buttonSleep_Click(object sender, RoutedEventArgs e)
+        {
+            Logic.Send(Logic.GetAntiMagicPacket(textMAC.Text), textIP.Text);
+        }
+
+        private void textIP_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            List<string> list = Settings.Load(Path);
+            list[0] = textIP.Text;
+            Settings.Save(Path, list);
+        }
+
+        private void textMAC_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            List<string> list = Settings.Load(Path);
+            list[1] = textMAC.Text;
+            Settings.Save(Path, list);
+        }
+    }
+}
